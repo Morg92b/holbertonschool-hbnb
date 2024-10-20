b366f0b18305c04c864ab00b5358aadee48366b8
bisoukeiko <87bisou@gmail.com>
2024-10-20 17:35:43 +0200

    add ValidationError, Validator_amenity

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,4 +1,27 @@
-from base_model import BaseModel
+from app.models.base_model import BaseModel
+
+class ValidationError(Exception):
+    pass
+
+
+class Validator_amenity:
+    @staticmethod
+    def validate_presence(value, field_name):
+        if not value:
+            raise ValidationError(f"{field_name} is required")
+
+    @staticmethod
+    def validate_length(value, field_name, max_length):
+        if len(value) > max_length:
+            raise ValidationError(f"{field_name} must be less than or equal to {max_length} characters")
+
+    @staticmethod
+    def validate_amenity(amenity_data):
+        """ Validating user attributes """
+        for key, value in amenity_data.items():
+            if key == 'name':
+                Validator_amenity.validate_presence(value, "name")
+                Validator_amenity.validate_length(value, "name", 50)
 
 
 class Amenity(BaseModel):
@@ -10,26 +33,8 @@ class Amenity(BaseModel):
             "name": self.name
         }
 
-        Amenity.verification_attr(amenity_data)
-
-        # if not self.name:
-        #     raise ValueError("name is required")
-
-        # if len(self.name) > 50:
-        #     raise ValueError("name must be less than or equal to 50 characters")
-
-    @classmethod
-    def verification_attr(cls, dict_attr):
-        if "name" in dict_attr:
-            if not dict_attr["name"]:
-                raise ValueError("name is required")
-
-            if len(dict_attr["name"]) > 50:
-                raise ValueError("name must be less than or equal to 50 characters")
-
-    def save(self):
-        super().save()
+        Validator_amenity.validate_amenity(amenity_data)
 
     def update(self, data):
-        Amenity.verification_attr(data)
+        Validator_amenity.validate_amenity(data)
         super().update(data)
