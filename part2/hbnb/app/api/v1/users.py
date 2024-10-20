401490137dcefb8263d4292c9dbe03f5b97d3e44
bisoukeiko <87bisou@gmail.com>
2024-10-20 16:34:15 +0200

    add user_model2

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -5,10 +5,16 @@ from app.models.user import ValidationError
 api = Namespace('users', description='User operations')
 
 # Define the user model for input validation and documentation
-user_model = api.model('User', {
+user_model1 = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
     'email': fields.String(required=True, description='Email of the user')
+})
+
+user_model2 = api.model('User', {
+    'first_name': fields.String(required=False, description='First name of the user'),
+    'last_name': fields.String(required=False, description='Last name of the user'),
+    'email': fields.String(required=False, description='Email of the user')
 })
 
 facade = HBnBFacade()
@@ -26,7 +32,7 @@ class UserList(Resource):
         return [{'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email} for user in user_list], 200
 
 
-    @api.expect(user_model, validate=True)
+    @api.expect(user_model1, validate=True)
     @api.response(201, 'User successfully created')
     @api.response(400, 'Email already registered')
     @api.response(400, 'Invalid input data')
@@ -61,7 +67,7 @@ class UserResource(Resource):
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
 
-    @api.expect(user_model, validate=True)
+    @api.expect(user_model2, validate=True)
     @api.response(200, 'User details successfully updated')
     @api.response(404, 'User not found')
     @api.response(400, 'Invalid input data')
