3d8319c6053eee16a132f5a174f8894de3d6ab49
bisoukeiko <87bisou@gmail.com>
2024-10-20 17:33:18 +0200

    add Placeholder for logic to create, read, update the amenity

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
-from app.models.user import User, Validator
+from app.models.user import User, Validator_user
+from app.models.amenity import Amenity, Validator_amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -8,26 +9,51 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
+    # Placeholder for logic to create, read, update the user
     def create_user(self, user_data):
+        """ Placeholder method for creating a user """
         user = User(**user_data)
         self.user_repo.add(user)
         return user
 
     def get_user(self, user_id):
+        """ Placeholder method for geting the user infomation"""
         return self.user_repo.get(user_id)
 
     def get_all_user(self):
+        """ Placeholder method for geting the all user's infomation """
         return self.user_repo.get_all()
 
     def put_user(self, user_id, data):
-        Validator.validate_user(data)
+        """ Placeholder method for updating the user infomation"""
+        Validator_user.validate_user(data)
         self.user_repo.update(user_id, data)
         return self.get_user(user_id)
 
     def get_user_by_email(self, email):
+        """ Placeholder method for geting the user infomation by email """
         return self.user_repo.get_by_attribute('email', email)
 
+    # Placeholder for logic to create, read, update the amenity
+    def create_amenity(self, amenity_data):
+        """ Placeholder for logic to create an amenity """
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        """ Placeholder for logic to retrieve an amenity by ID """
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        """ Placeholder for logic to retrieve all amenities """
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        """ Placeholder for logic to update an amenity """
+        Validator_amenity.validate_amenity(amenity_data)
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.get_amenity(amenity_id)
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
