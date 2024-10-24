3a66898aed27bd3bd2d647e0a7e1aabd5691b4e2
bisoukeiko <87bisou@gmail.com>
2024-10-24 22:53:43 +0200

    fix code update method: add velification attribute

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -9,15 +9,14 @@ class Amenity(BaseModel):
             raise ValueError("The name of the equipment is mandatory and must not exceed 50 characters")
         self.name = name
 
-    def save(self):
-        super().save()
+    # def save(self):
+    #     super().save()
 
     def update(self, data):
         for key, value in data.items():
-            if key == "name" and value:
-                if len(value) > 50:
+            if key == "name":
+                if not value or len(value) > 50:
                     raise ValueError("The equipment name must not exceed 50 characters")
-                self.name = value
         super().update(data)
 
     
