d1cf976793a67e0e62a7480183fc7a350eb270b2
bisoukeiko <87bisou@gmail.com>
2024-11-12 00:22:16 +0100

    add admin for test

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -7,6 +7,7 @@ from app.api.v1.places import api as place_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 from app.api.v1.protected import api as protected_ns
+from app.api.v1.admin import api as admin_ns
 from .extensions import bcrypt, jwt
 
 
@@ -27,6 +28,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     place_ns.facade = app.config['FACADE']
     reviews_ns.facade = app.config['FACADE']
     auth_ns.facade = app.config['FACADE']
+    admin_ns.facade = app.config['FACADE']
 
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
@@ -38,6 +40,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(protected_ns, path='/api/v1/protected')
+    api.add_namespace(admin_ns, path='/api/v1/admin')
 
 
     return app
