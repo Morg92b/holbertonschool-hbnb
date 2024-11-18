75b8f5cc22d3a14ba43dbccfac8ce65c09edac45
bisoukeiko <87bisou@gmail.com>
2024-11-18 12:19:24 +0100

    fix code: comentout line for admin entity

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -7,7 +7,7 @@ from app.api.v1.places import api as place_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 from app.api.v1.protected import api as protected_ns
-from app.api.v1.admin import api as admin_ns
+# from app.api.v1.admin import api as admin_ns
 from .extensions import bcrypt, jwt, db 
 
 
@@ -29,7 +29,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     place_ns.facade = app.config['FACADE']
     reviews_ns.facade = app.config['FACADE']
     auth_ns.facade = app.config['FACADE']
-    admin_ns.facade = app.config['FACADE']
+    # admin_ns.facade = app.config['FACADE']
 
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
@@ -41,7 +41,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(protected_ns, path='/api/v1/protected')
-    api.add_namespace(admin_ns, path='/api/v1/admin')
+    # api.add_namespace(admin_ns, path='/api/v1/admin')
 
 
 
