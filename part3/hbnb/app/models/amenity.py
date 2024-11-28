6e82f52370a8aaec734ec4bacb1142dd8b40c4cc
bisoukeiko <87bisou@gmail.com>
2024-11-29 00:11:36 +0100

    add PlaceAmenity class, relationship with tb_place_amenity

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -1,10 +1,13 @@
 from .base_model import BaseModel
 from app.extensions import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
     __tablename__ = 'TB_AMENITY'
 
     name = db.Column(db.String(50), nullable=False)
+    place_amenity = relationship('PlaceAmenity',  backref='tb_place_amenity', lazy=True)
 
 
     def __init__(self, name):
@@ -26,3 +29,25 @@ class Amenity(BaseModel):
             'id': self.id,
             'name': self.name,
         }
+    
+
+
+class PlaceAmenity(BaseModel):
+    __tablename__ = 'TB_PLACE_AMENITY'
+
+    place_id = db.Column(db.String(36), ForeignKey('TB_PLACE.id'), nullable=False)
+    amenity_id = db.Column(db.String(36), ForeignKey('TB_AMENITY.id'), nullable=False)
+
+    amenity = relationship('Amenity', backref='placeamenity', lazy=True)
+
+    def __init__(self, place_id, amenity_id):
+
+        self.place_id = place_id
+        self.amenity_id = amenity_id
+
+# # Association table for Place et Amenity relationship
+# place_amenity = db.Table('TB_PLACE_AMENITY',
+#     Column('place_id', String, ForeignKey('TB_PLACE.id'), primary_key=True),
+#     Column('amenity_id', String, ForeignKey('TB_AMENITY.id'), primary_key=True)
+# )
+
