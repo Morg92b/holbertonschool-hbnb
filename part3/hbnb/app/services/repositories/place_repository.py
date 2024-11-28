9246332b397bf199cb1df60ae84736b50875dd96
bisoukeiko <87bisou@gmail.com>
2024-11-29 00:19:12 +0100

    remove *_repository.py

--- part3/hbnb/app/services/repositories/place_repository.py
+++
@@ -1,7 +0,0 @@
-from app.models.place import Place
-from app.extensions import db 
-from app.persistence.repository import SQLAlchemyRepository
-
-class PlaceRepository(SQLAlchemyRepository):
-    def __init__(self):
-        super().__init__(Place)
