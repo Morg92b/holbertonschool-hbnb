3a5502372508c70133d70c6e61a78e5b8338bf58
bisoukeiko <87bisou@gmail.com>
2024-11-29 00:08:32 +0100

    add import CORS

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -7,13 +7,16 @@ from app.api.v1.places import api as place_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 from app.api.v1.protected import api as protected_ns
-# from app.api.v1.admin import api as admin_ns
-from .extensions import bcrypt, jwt, db 
+from app.api.v1.admin import api as admin_ns
+from .extensions import bcrypt, jwt, db
+from flask_cors import CORS
 
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    
     app.config.from_object(config_class)
+    cors = CORS(app, resources={r"/*": {"origins": "http://localhost:5500", "allow_headers": "Content-Type"}})
 
     bcrypt.init_app(app)
     jwt.init_app(app)
@@ -29,7 +32,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     place_ns.facade = app.config['FACADE']
     reviews_ns.facade = app.config['FACADE']
     auth_ns.facade = app.config['FACADE']
-    # admin_ns.facade = app.config['FACADE']
+    admin_ns.facade = app.config['FACADE']
 
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
@@ -41,7 +44,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(protected_ns, path='/api/v1/protected')
-    # api.add_namespace(admin_ns, path='/api/v1/admin')
+    api.add_namespace(admin_ns, path='/api/v1/admin')
 
 
 
